a26aa0e757d25534257a9361d558a21c566bad7e
Mixanomizer <mixanomizer4@mail.ru>
2025-10-30 16:41:52 +0500

    Добавил функциональность кнопкам удалить и выполнено

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const inputElement = document.getElementById('title')
 const createBtn = document.getElementById('create')
 const listElement = document.getElementById('list')
-// const notes= ['Изучить новую тему JS', 'Выполнить проект в Anylogic']
 
+// добавление новых заметок
 createBtn.onclick=function(){
     if (inputElement.value.length===0){
         return
@@ -17,21 +17,25 @@ createBtn.onclick=function(){
 }
 
 function render(){
-     listElement.innerHTML='';
+     listElement.innerHTML=''
+     if (notes.length===0){
+        listElement.innerHTML='Нет элементов'
+     }
     for (let note of notes) {
         listElement.insertAdjacentHTML(
         'beforeend',
         getNoteTemplate(note, notes.indexOf(note)))        
     }    
+    
 }
     
 
 function getNoteTemplate(note, index){
     return `<li class="pading">
-          <span class="${note.comleted? 'text-decoration-line-trought': ''}">${note.title} i=${index}</span>
+          <span class="${note.completed? 'text-decoration-line-trought': ''}">${note.title}</span>
           <span class="btn-position">
-            <span  class="btn-status btn-${note.comleted? 'completed': 'success'} btn">&check;</span>
-            <span class="btn-status btn btn-danger">&times;</span>
+            <span  class="btn-status btn-${note.completed? 'completed': 'success'} btn" data-index=${index} data-type="completed">&check;</span>
+            <span class="btn-status btn btn-danger" data-index=${index} data-type="remove">&times;</span>
           </span>
         </li>`
 }
@@ -40,12 +44,32 @@ function getNoteTemplate(note, index){
 
 const notes = [
     {
-        title: 'Изучить новую тему JS',
-        comleted: true,
+        title: "Изучить новую тему JS",
+        completed: true
     },
     {
-        title: 'Изучить новую тему JS',
-        comleted: false,
+        title: "Выполнить проект в Anylogic",
+        completed: false
+    },
+    {
+        title: "Забрать пропуск",
+        completed: false,
     },
 ]
+
+
+//получаем информацию по какому эл-менту сделан клик
+listElement.onclick= function(event){
+    
+    if(event.target.dataset.index){
+        const index= parseInt(event.target.dataset.index);
+        const type= event.target.dataset.type;
+         if (type==="completed"){
+            notes[index].completed=!notes[index].completed
+        } else  if (type==="remove"){
+            notes.splice(index, 1)
+        }
+    }
+   render()
+}
 render()
